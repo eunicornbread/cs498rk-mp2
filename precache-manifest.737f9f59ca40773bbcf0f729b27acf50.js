db6a5bfe083f15e67ec010bf7c5c0a860dcb2024
eunicornbread <40338933+eunicornbread@users.noreply.github.com>
2019-03-11 22:53:17 -0500

    Updates

---
+++ precache-manifest.737f9f59ca40773bbcf0f729b27acf50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc97c56da575cf011ab0",
+    "url": "/cs498rk-mp2/static/js/runtime~main.7aa1c6c4.js"
+  },
+  {
+    "revision": "e3a6a4839e43d90de621",
+    "url": "/cs498rk-mp2/static/js/main.faa87201.chunk.js"
+  },
+  {
+    "revision": "a8f449ce8c0fa3793586",
+    "url": "/cs498rk-mp2/static/js/2.7f3115ab.chunk.js"
+  },
+  {
+    "revision": "e3a6a4839e43d90de621",
+    "url": "/cs498rk-mp2/static/css/main.26f8c525.chunk.css"
+  },
+  {
+    "revision": "a8f449ce8c0fa3793586",
+    "url": "/cs498rk-mp2/static/css/2.23720a9b.chunk.css"
+  },
+  {
+    "revision": "e1861d2f0a5ab75e3e682271f089482b",
+    "url": "/cs498rk-mp2/index.html"
+  }
+];
